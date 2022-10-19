3b674fc8b816d906ac6741eba13a38aee9d69654
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2022-10-19 19:01:08 +0100

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
-	char alpha;
-
+char alpha;
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		_putchar (alpha);
+	_putchar (alpha);
 	}
 	_putchar('\n');
 }
