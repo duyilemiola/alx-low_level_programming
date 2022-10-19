9eac819268527d7d123eda7844497715441de957
duyilemiola <ola.omo51@yahoo.com>
2022-10-19 12:07:10 -0700

    kkk

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in lowercase
- */
+ * print_alphabet - Prints the alphabet in lowercase 
+*/
 void print_alphabet(void)
 {
 	char alpha;
 	
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	 	_putchar(alpha);
+		_putchar(alpha);
 	_putchar('\n');
 }
