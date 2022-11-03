74eb2d5b6739f6d11949df8bb50fa9029a289abb
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2022-11-04 00:37:59 +0100

    strlen_

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - returns the length of a string
- * @s: pointer input
+ * @s: string to calculate the length of
  *
- * Return: the length of a string
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return (0);
-        }
-        s++;
-        return (_strlen_recursion(s) + 1);
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
