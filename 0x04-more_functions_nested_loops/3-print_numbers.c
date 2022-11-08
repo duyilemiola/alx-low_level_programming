42d60cf0d4aff1362fe30d1b009065e269fc373f
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2022-11-08 05:33:09 +0100

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
