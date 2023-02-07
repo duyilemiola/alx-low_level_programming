affe23c07306343d76ca5850020c82b9e1fc4ad8
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-02-07 07:47:44 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,48 @@
-#include "hash_tables.h"
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
- * hash_table_create - Creates a hash table.
- * @size: size of the array that holds hashtable keys.
+ * struct hash_node_s - Node of a hash table
  *
- * Return: on success-Pointer to newly created hash table
- *         on failure-Return NULL.
- **/
-hash_table_t *hash_table_create(unsigned long int size)
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
 {
-	hash_table_t *hash_t;
-	unsigned long int i;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
 
-	hash_t = malloc(sizeof(hash_table_t));
-	if (hash_t == NULL)
-		return (NULL);
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
 
-	hash_t->size = size;
-	hash_t->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_t->array == NULL)
-		return (NULL);
-	/*Initialize the hashtable to NULL*/
-	for (i = 0; i < size; i++)
-		hash_t->array[i] = NULL;
-	return (hash_t);
-}
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_node_t *create_new_node (const char *key, const char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+void free_hash_list(hash_node_t *head);
+
+#endif
